75c7c6fd73c357763c6880396c05fc4d821b2814
alexey-m <alexey-m@pfm.team>
2026-07-03 14:58:09 +0300

    Initial Commit

---
+++ NeckFingering/NeckFingeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeckFingeringApp.swift
+//  NeckFingering
+//
+//  Created by Алексей Моторин on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NeckFingeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
